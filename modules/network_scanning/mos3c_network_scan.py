16d306a1a77ba50157211c7f04f296dab3779645
mos3c <elijahesan2@gmail.com>
2025-06-15 12:18:31 +0100

    updated the mos3c_network_scan, added an exit option

--- modules/network_scanning/mos3c_network_scan.py
+++ modules/network_scanning/mos3c_network_scan.py
@@ -3,13 +3,12 @@ import subprocess
 import os
 import sys
 
-init()
+init(autoreset=True)
 
 def clear_screen():
-    os.system('clear')
+    os.system('clear' if os.name != 'nt' else 'cls')
 
 def Scan_menu():
-
     while True:
         clear_screen()
         print(Fore.BLUE + Style.BRIGHT + "─" * 65)
@@ -17,16 +16,18 @@ def Scan_menu():
         print(Fore.BLUE + Style.BRIGHT + "─" * 65)
 
         options = [
-            "1]  Performs a full port scan using Nmap. ",
-            "2]  Scans for open ports and services using Masscan. ",
-
+            "1]  Performs a full port scan using Nmap.",
+            "2]  Scans for open ports and services using Masscan.",
+            "3]  Return to Previous Menu / Exit."
         ]
 
         for option in options:
             print(f"{Fore.BLUE}│ {Fore.WHITE}{option.ljust(62)}{Fore.BLUE} │")
 
+        print(Fore.BLUE + "─" * 65)
+
         try:
-            choice = input(Fore.BLUE + Style.BRIGHT + "SELECT SQLi OPTION: ").strip()
+            choice = input(Fore.BLUE + Style.BRIGHT + "\nSELECT OPTION: ").strip()
 
             if choice == '1':
                 print(Fore.GREEN + "[+] Launching Nmap Full Scan...")
@@ -36,6 +37,7 @@ def Scan_menu():
                 else:
                     subprocess.run([sys.executable, script_path])
                 input(Fore.BLUE + "\nPress Enter to continue...")
+
             elif choice == '2':
                 print(Fore.GREEN + "[+] Launching Masscan Scan...")
                 script_path = "modules/network_scanning/masscan.py"
@@ -44,10 +46,15 @@ def Scan_menu():
                 else:
                     subprocess.run([sys.executable, script_path])
                 input(Fore.BLUE + "\nPress Enter to continue...")
-                
+
+            elif choice == '3':
+                print(Fore.YELLOW + "[*] Returning to previous menu...")
+                break
+
             else:
-                print(Fore.RED + "Invalid option, try again.")
+                print(Fore.RED + "[-] Invalid option, try again.")
                 input(Fore.BLUE + "\nPress Enter to continue...")
+
         except KeyboardInterrupt:
             print(Fore.RED + "\n[!] Operation cancelled by user.")
             input(Fore.BLUE + "\nPress Enter to continue...")
